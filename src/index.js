f6deee1d0cf96113ce80a45e91cbf9e7a8f7f867
Andrew <armartin1998@gmail.com>
2023-03-10 22:17:12 -0500

    Complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTask = document.getElementById('new-task-description')
+  const form = document.querySelector('form')
+  newTask.id = 'newTaskDescription'
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    listTask(e.target.newTaskDescription.value)
+    form.reset()
+  })
+
+  function listTask(task){
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'X'
+    li.textContent = `${task}  `
+    li.appendChild(btn)
+    document.getElementById('tasks').appendChild(li)
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+
 });
